6736936ef98b2b71f9041a17d97a3e0d59065621
dinhanhhhh <truongdinhanh27@gmail.com>
2023-02-17 10:22:03 +0700

    Cap nhat chuc nang

---
+++ MyApp/src/main/java/com/mycompany/myapp/MyApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myapp;
+
+/**
+ *
+ * @author Admin
+ */
+public class MyApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
